9f9231a8fb96c30a572ee664f1a9fad5f326fb70
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:35:00 +0700

    Update MainActivity.kt

--- android/app/src/main/java/com/craftdroid/MainActivity.kt
+++ android/app/src/main/java/com/craftdroid/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import com.craftdroid.ui.App
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
