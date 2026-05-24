e4976d8c096ddc904ae4e31af7604f5e01e4a197
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:57:01 +0700

    Create TypoGraphy.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/theme/TypoGraphy.kt
@@ -0,0 +1,6 @@
+// android/app/src/main/java/com/craftdroid/ui/theme/Typography.kt
+package com.craftdroid.ui.theme
+
+import androidx.compose.material3.Typography
+
+val CraftTypography = Typography()
