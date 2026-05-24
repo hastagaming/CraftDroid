a6015f9dacf34bb9801ca785ad7a17833e23be2e
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 18:05:22 +0700

    Create ConfigureServerScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/wizard/ConfigureServerScreen.kt
@@ -0,0 +1,10 @@
+// android/app/src/main/java/com/craftdroid/ui/wizard/ConfigureServerScreen.kt
+package com.craftdroid.ui.wizard
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ConfigureServerScreen() {
+    Text("Configure Server")
+}
