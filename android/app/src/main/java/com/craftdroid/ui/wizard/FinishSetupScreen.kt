8f2004345a9a4ce26570ac7d0987a85eeb035941
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 18:06:27 +0700

    Create FinishSetupScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/wizard/FinishSetupScreen.kt
@@ -0,0 +1,10 @@
+// android/app/src/main/java/com/craftdroid/ui/wizard/FinishSetupScreen.kt
+package com.craftdroid.ui.wizard
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FinishSetupScreen() {
+    Text("Finish Setup")
+}
