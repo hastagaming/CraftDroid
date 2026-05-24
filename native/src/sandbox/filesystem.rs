6364087afc3cb6f01ed40f79847b00574856c902
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:46:56 +0700

    Update filesystem.rs

--- native/src/sandbox/filesystem.rs
+++ native/src/sandbox/filesystem.rs
@@ -1,3 +1,9 @@
-pub fn create_container(path: &str) {
-    println!("Creating container at {}", path);
+use std::fs;
+
+pub fn create_container(name: &str) {
+    let base = format!("containers/{}", name);
+
+    fs::create_dir_all(format!("{}/logs", base)).unwrap();
+    fs::create_dir_all(format!("{}/plugins", base)).unwrap();
+    fs::create_dir_all(format!("{}/worlds", base)).unwrap();
 }
