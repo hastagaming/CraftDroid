86c33f283af0c111389068596fa85516a26218e7
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:44:46 +0700

    Update CraftDaemonService.kt

--- android/app/src/main/java/com/craftdroid/daemon/CraftDaemonService.kt
+++ android/app/src/main/java/com/craftdroid/daemon/CraftDaemonService.kt
@@ -1,16 +1,51 @@
 package com.craftdroid.daemon
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.craftdroid.R
 
 class CraftDaemonService : Service() {
 
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        val notification: Notification = NotificationCompat.Builder(this, "craftdroid")
+            .setContentTitle("CraftDroid Runtime")
+            .setContentText("Minecraft runtime is active")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build()
+
+        startForeground(1, notification)
+
         return START_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel(
+                "craftdroid",
+                "CraftDroid Service",
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
     }
 }
