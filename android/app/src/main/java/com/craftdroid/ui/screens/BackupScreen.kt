c43e5efe43c1d48c1b6a9e6cec06b4da140c61cd
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 15:03:44 +0700

    Create BackupScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/screens/BackupScreen.kt
@@ -0,0 +1,11 @@
+// android/app/src/main/java/com/craftdroid/ui/screens/BackupScreen.kt
+package com.craftdroid.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun BackupScreen(navController: NavHostController) {
+    Text("Backup Screen")
+}
