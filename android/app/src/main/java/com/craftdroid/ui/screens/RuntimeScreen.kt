247405d64ec42a5c90021728220841eb73122a83
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 15:02:58 +0700

    Create RuntimeScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/screens/RuntimeScreen.kt
@@ -0,0 +1,11 @@
+// android/app/src/main/java/com/craftdroid/ui/screens/RuntimeScreen.kt
+package com.craftdroid.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun RuntimeScreen(navController: NavHostController) {
+    Text("Runtime Screen")
+}
