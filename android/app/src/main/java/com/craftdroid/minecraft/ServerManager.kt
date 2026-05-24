d37eea5b3ca33406733f162e1ea48c61c6d5ae52
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:45:20 +0700

    Update ServerManager.kt

--- android/app/src/main/java/com/craftdroid/minecraft/ServerManager.kt
+++ android/app/src/main/java/com/craftdroid/minecraft/ServerManager.kt
@@ -1,16 +1,30 @@
 package com.craftdroid.minecraft
 
+import java.io.File
+
 class ServerManager {
 
     fun createServer(name: String) {
-        println("Creating server: $name")
+
+        val container = File("/data/data/com.craftdroid/containers/$name")
+
+        if (!container.exists()) {
+            container.mkdirs()
+        }
+
+        File(container, "logs").mkdirs()
+        File(container, "plugins").mkdirs()
+        File(container, "worlds").mkdirs()
+        File(container, "server").mkdirs()
     }
 
-    fun startServer() {
-        println("Starting server")
+    fun startPocketMine(name: String) {
+        Runtime.getRuntime().exec(
+            "sh scripts/start_container.sh $name"
+        )
     }
 
-    fun stopServer() {
-        println("Stopping server")
+    fun stopServer(process: Process) {
+        process.destroy()
     }
 }
