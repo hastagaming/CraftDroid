65ebb2b0010310da72b51aa0ef42e1ea435c363d
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 18:05:57 +0700

    Create InstallRuntimeScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/wizard/InstallRuntimeScreen.kt
@@ -0,0 +1,10 @@
+// android/app/src/main/java/com/craftdroid/ui/wizard/InstallRuntimeScreen.kt
+package com.craftdroid.ui.wizard
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InstallRuntimeScreen() {
+    Text("Install Runtime")
+}
