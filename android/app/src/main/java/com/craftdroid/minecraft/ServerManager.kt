5d24d76564078eafe025e28d59d1dfdc28970c1d
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:20:18 +0700

    Create minecraft/ServerManager.kt

---
+++ android/app/src/main/java/com/craftdroid/minecraft/ServerManager.kt
@@ -0,0 +1,16 @@
+package com.craftdroid.minecraft
+
+class ServerManager {
+
+    fun createServer(name: String) {
+        println("Creating server: $name")
+    }
+
+    fun startServer() {
+        println("Starting server")
+    }
+
+    fun stopServer() {
+        println("Stopping server")
+    }
+}
