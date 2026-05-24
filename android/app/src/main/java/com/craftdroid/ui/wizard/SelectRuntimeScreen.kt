4911f8167596aa22825dee705d9094cbc11badf8
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 15:05:57 +0700

    Create SelectRuntimeScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/wizard/SelectRuntimeScreen.kt
@@ -0,0 +1,10 @@
+// android/app/src/main/java/com/craftdroid/ui/wizard/SelectRuntimeScreen.kt
+package com.craftdroid.ui.wizard
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SelectRuntimeScreen() {
+    Text("Select Runtime")
+}
