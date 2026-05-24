0bb518090aa2e7dd10fb80776c72dc7b2022d351
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 15:01:35 +0700

    Create screens/SplashScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/screens/SplashScreen.kt
@@ -0,0 +1,11 @@
+// android/app/src/main/java/com/craftdroid/ui/screens/PluginScreen.kt
+package com.craftdroid.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun PluginScreen(navController: NavHostController) {
+    Text("Plugin Screen")
+}
