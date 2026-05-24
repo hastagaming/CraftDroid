727d50bc8c15ab4206ccd450cc396e7ab6a9d9db
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 15:02:38 +0700

    Create FileManagerScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/screens/FileManagerScreen.kt
@@ -0,0 +1,11 @@
+// android/app/src/main/java/com/craftdroid/ui/screens/FileManagerScreen.kt
+package com.craftdroid.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun FileManagerScreen(navController: NavHostController) {
+    Text("File Manager Screen")
+}
