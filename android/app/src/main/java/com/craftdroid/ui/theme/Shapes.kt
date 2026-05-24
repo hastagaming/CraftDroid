81399f06b9979749ec5dc5fedebc3656f5e944d8
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:56:31 +0700

    Create Shapes.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/theme/Shapes.kt
@@ -0,0 +1,12 @@
+// android/app/src/main/java/com/craftdroid/ui/theme/Shapes.kt
+package com.craftdroid.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val CraftShapes = Shapes(
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(18.dp),
+    large = RoundedCornerShape(24.dp)
+)
