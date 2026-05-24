11f67616811948c041b134c4fa8b15894debcd29
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:35:26 +0700

    Update App.kt

--- android/app/src/main/java/com/craftdroid/ui/App.kt
+++ android/app/src/main/java/com/craftdroid/ui/App.kt
@@ -3,13 +3,16 @@ package com.craftdroid.ui
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
 import com.craftdroid.ui.navigation.NavGraph
 
 @Composable
 fun App() {
+    val navController = rememberNavController()
+
     MaterialTheme {
         Surface {
-            NavGraph()
+            NavGraph(navController)
         }
     }
 }
