97366de1a78a2997156fb240ff32a1f5f4df7a0d
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 15:05:08 +0700

    Create SelectEditionScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/wizard/SelectEditionScreen.kt
@@ -0,0 +1,10 @@
+// android/app/src/main/java/com/craftdroid/ui/wizard/SelectEditionScreen.kt
+package com.craftdroid.ui.wizard
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SelectEditionScreen() {
+    Text("Select Edition")
+}
