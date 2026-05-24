63298ffeb4bb10ccef3ed5456bbb46244385c379
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 15:04:08 +0700

    Create NetworkScreen.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/screens/NetworkScreen.kt
@@ -0,0 +1,11 @@
+// android/app/src/main/java/com/craftdroid/ui/screens/NetworkScreen.kt
+package com.craftdroid.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun NetworkScreen(navController: NavHostController) {
+    Text("Network Screen")
+}
