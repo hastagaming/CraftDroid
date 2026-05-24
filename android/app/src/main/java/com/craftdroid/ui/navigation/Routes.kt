7a91d1a339d50d577b575ae2ba4a1f9fb8c90f7d
hastagaming <rip.hastagaming@gmail.com>
2026-05-24 14:40:08 +0700

    Create navigation/Routes.kt

---
+++ android/app/src/main/java/com/craftdroid/ui/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.craftdroid.ui.navigation
+
+object Routes {
+    const val HOME = "home"
+    const val CREATE_SERVER = "create_server"
+    const val DASHBOARD = "dashboard"
+    const val CONSOLE = "console"
+}
